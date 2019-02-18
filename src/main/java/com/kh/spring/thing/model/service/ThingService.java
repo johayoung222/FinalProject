8e6f1a9ea7415003f38cea4d238f9b425b8ac59f
javajopro <whrmwl123134@gmail.com>
2019-02-18 17:58:16 +0900

    판매 등록중

--- src/main/java/com/kh/spring/thing/model/service/ThingService.java
+++ src/main/java/com/kh/spring/thing/model/service/ThingService.java
@@ -1,5 +1,11 @@
 package com.kh.spring.thing.model.service;
 
+import java.util.List;
+
+import com.kh.spring.thing.model.vo.Category;
+
 public interface ThingService {
+
+	List<Category> selectCategorys();
 
 }
