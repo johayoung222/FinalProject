d16d6b45c12cce9d95fdbce4417453cf802e463a
kyungkook <803kkk@hanmail.net>
2019-02-18 19:25:37 +0900

    구매 화면 구현 및 우편번호 api, 결제 api 적용

--- src/main/java/com/kh/spring/thing/controller/ThingController.java
+++ src/main/java/com/kh/spring/thing/controller/ThingController.java
@@ -1,5 +1,6 @@
 package com.kh.spring.thing.controller;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +9,8 @@ import com.kh.spring.thing.model.service.ThingService;
 
 @Controller
 public class ThingController {
+	
+	Logger logger = Logger.getLogger(getClass());
 	
 	@Autowired
 	ThingService thingService;
@@ -29,6 +32,11 @@ public class ThingController {
 		return "";
 	}
 	
+	@RequestMapping("/item/perchase")
+	public String movePerchase() {
+		
+		return "item/perchase";
+	}
 	
 	
 	
