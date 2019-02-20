28ec363ef1dd3d6ae9c48198e932f407626d6f1e
정현빈 <soads1@naver.com>
2019-02-20 21:13:40 +0900

    1:1

--- src/main/java/com/kh/spring/customercenter/controller/CustomercenterController.java
+++ src/main/java/com/kh/spring/customercenter/controller/CustomercenterController.java
@@ -11,6 +11,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.kh.spring.customercenter.model.service.CustomerService;
+import com.kh.spring.customercenter.model.vo.Board;
+import com.kh.spring.memo.model.vo.Memo;
+
 
 @Controller
 public class CustomercenterController {
@@ -68,6 +71,83 @@ public class CustomercenterController {
 		mav.addObject("numPerPage", numPerPage);
 		
 		
+		return mav;
+	}
+	
+	
+	//관리자일시 공지사항등록
+	@RequestMapping("/customercenter/insertNews.do")
+	public ModelAndView insertNews(Board board,ModelAndView mav) {
+		
+			logger.debug("board="+board);
+		
+		
+			//2. 업무로직
+			int result = customerService.insertNews(board);
+			logger.debug("cont result"+result);	
+			
+			//3. 뷰단처리
+			String loc = "/customercenter/ccnews.do";
+			String msg = "";
+			
+			if(result>0) {
+				msg = "게시물 등록 성공";
+			}else {
+				msg= "게시물 등록 실패";
+			}
+			
+			
+			mav.addObject("loc", loc);
+			mav.addObject("msg", msg);
+			mav.setViewName("common/msg");
+		
+		return mav;
+	}
+	
+	@RequestMapping("/customercenter/updateNews.do")
+	public ModelAndView updateNews(Board board, ModelAndView mav) {
+		//logger.debug("board="+board);
+		
+		int result = customerService.updateNews(board);
+		logger.debug("cont result"+result);	
+		
+		//3. 뷰단처리
+		String loc = "/customercenter/ccnews.do";
+		String msg = "";
+		
+		if(result>0) {
+			msg = "게시물 수정 성공";
+		}else {
+			msg= "게시물 수정 실패";
+		}
+		
+		
+		mav.addObject("loc", loc);
+		mav.addObject("msg", msg);
+		mav.setViewName("common/msg");
+	
+	return mav;
+	}
+	
+	@RequestMapping("/customercenter/deleteNews.do")
+	public ModelAndView deleteNews(Board board, ModelAndView mav) {
+		logger.debug("board="+board);
+		
+		int result = customerService.deleteNews(board);
+		
+		String loc = "/customercenter/ccnews.do";
+		String msg = "";
+		
+		if(result>0) {
+			msg = "게시물 삭제 성공";
+		}else {
+			msg= "게시물 삭제 실패";
+		}
+		
+		mav.addObject("loc", loc);
+		mav.addObject("msg", msg);
+		mav.setViewName("common/msg");
+
 		return mav;
 	}
 
