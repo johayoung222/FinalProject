68cfb17afe34747d7b8af12b6317fa33596b5f1d
HaYoung Jo <johayoung222@naver.com>
2019-02-20 16:55:56 +0900

    Auction Writer Commit

--- src/main/java/com/kh/spring/auction/model/service/AuctionService.java
+++ src/main/java/com/kh/spring/auction/model/service/AuctionService.java
@@ -2,8 +2,12 @@ package com.kh.spring.auction.model.service;
 
 import java.util.List;
 
+import com.kh.spring.thing.model.vo.CategoryMacro;
+
 public interface AuctionService {
 
 	List<String> selectctgList();
+
+	List<CategoryMacro> selectctgmlist(String ctgcode);
 
 }
