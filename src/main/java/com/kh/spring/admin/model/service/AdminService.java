28b916c645b03a0cfdfd10136902dbcfeff2d143
Min_seong <withness77@gmail.com>
2019-02-26 17:35:11 +0900

    관리자> 회원리스트조회/ 결제된상품리스트 / 상품리스트 검색 페이징 구현

--- src/main/java/com/kh/spring/admin/model/service/AdminService.java
+++ src/main/java/com/kh/spring/admin/model/service/AdminService.java
@@ -10,39 +10,33 @@ import com.kh.spring.thing.model.vo.ProductIo;
 import com.kh.spring.thing.model.vo.Regist;
 
 public interface AdminService {
-
+	
+	//회원 리스트 조회 
 	List<Map<String, String>> allMember(int cPage, int numPerPage);
 
 	int countallMember();
 	
-	List<Map<String, String>> paidProduct(int cPage, int numPerPage);
+	List<Map<String, String>> memberSearch(int cPage, int numPerPage, Map<String, String> map);
 
+	int countmemberSearch(Map<String, String> map);
+	
+	
+	//결제된 상품 리스트 
+	List<Map<String, String>> paidProduct(int cPage, int numPerPage);
+	
 	int countpaidProduct();
 	
+	List<Map<String, String>> paidProductSearch(int cPage, int numPerPage, Map<String, String> map);
+
+	int countpaidProductSearch(Map<String, String> map);
+	
+	
+	
+	//판매신청 리스트
 	List<Map<String, Object>> regist(int cPage, int numPerPage);
 
 	int countregist();
-
-	List<Map<String, String>> productList(int cPage, int numPerPage);
-
-	int countproductList();
 	
-	List<Map<String, String>> questionAnswer(int cPage, int numPerPage);
-
-	int countquestionAnswer();
-
-	List<Map<String, String>> acutionStatus(int cPage, int numPerPage);
-
-	int countauctionStatus();
-
-	List<Map<String, String>> reportList(int cPage, int numPerPage);
-
-	int countreportList();
-
-	int countpaidProductSearch2(Map<String, String> map);
-
-	List<ProductIo> paidProductSearch(int cPage, int numPerPage, Map<String, String> map);
-
 	List<CategoryMacro> categoryMa();
 
 	List<Category> categoryMi(String macro);
@@ -54,6 +48,45 @@ public interface AdminService {
 	int insertProduct(Map<String, Object> map);
 
 	void updateRegist(int registNo);
+	
+	
+	
+	//상품 리스트
+	List<Map<String, String>> productList(int cPage, int numPerPage);
+
+	int countproductList();
+	
+	List<Map<String, String>> productListSearch(int cPage, int numPerPage, Map<String, String> map);
+
+	int countproductListSearch(Map<String, String> map);
+	
+	
+	
+	
+	//1:1질문답변
+	List<Map<String, String>> questionAnswer(int cPage, int numPerPage);
+
+	int countquestionAnswer();
+
+	
+	//경매상품 현황
+	List<Map<String, String>> acutionStatus(int cPage, int numPerPage);
+
+	int countauctionStatus();
+
+	//신고접수 리스트
+	List<Map<String, String>> reportList(int cPage, int numPerPage);
+
+	int countreportList();
+
+	
+
+	
+
+	
+
+	
+
 
 
 
