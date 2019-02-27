dab5d8c7f4c47e3bf1feb46d3156d074d6d7f9c5
ck <ekzmdlsej@naver.com>
2019-02-27 15:32:19 +0900

    basket ㄱㄱ

--- src/main/java/com/kh/spring/basket/model/service/BasketServiceImpl.java
+++ src/main/java/com/kh/spring/basket/model/service/BasketServiceImpl.java
@@ -1,5 +1,8 @@
 package com.kh.spring.basket.model.service;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -8,7 +11,19 @@ import com.kh.spring.basket.model.dao.BasketDao;
 @Service
 public class BasketServiceImpl implements BasketService{
 
-	@Autowired BasketDao basketdao;
+	@Autowired BasketDao basketDao;
+
+	@Override
+	public List<Map<String, String>> basketList(int cPage, int numPerPage) {
+		// TODO Auto-generated method stub
+		return basketDao.basketList(cPage, numPerPage);
+	}
+
+	@Override
+	public int countbasketList() {
+		// TODO Auto-generated method stub
+		return basketDao.countbasketList();
+	}
 	
 	
 	
