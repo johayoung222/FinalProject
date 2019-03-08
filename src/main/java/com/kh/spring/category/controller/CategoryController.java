4c0b4bf15a05b3c227504c203f680593ca0c162c
kyungkook <803kkk@hanmail.net>
2019-03-08 19:26:00 +0900

    index 물품 뿌리기 및 url 연결들

--- src/main/java/com/kh/spring/category/controller/CategoryController.java
+++ src/main/java/com/kh/spring/category/controller/CategoryController.java
@@ -9,13 +9,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.kh.spring.auction.model.service.AuctionService;
 import com.kh.spring.category.model.service.CategoryService;
+import com.kh.spring.item.model.service.ItemService;
 import com.kh.spring.thing.model.vo.Product;
-import com.kh.spring.thing.model.vo.Regist;
 
 @Controller
 public class CategoryController {
@@ -28,73 +27,20 @@ public class CategoryController {
 	@Autowired
 	AuctionService auctionService;
 	
-	
-	
-	
-	@RequestMapping("/category/laptopPc")
-	public String laptopPc() {
-		
-		
-		
-		
-		
-		return "category/laptopPc";
-	}
-	@RequestMapping("/category/Phone")
-	public String Phone() {
-		
-		
-		
-		
-		
-		return "category/Phone";
-	}
-	@RequestMapping("/category/appliance")
-	public String appliance() {
-		
-		
-		
-		
-		
-		return "category/appliance";
-	}
-	@RequestMapping("/category/lifeFood")
-	public String lifeFood() {
-		
-		
-		
-		
-		
-		return "category/lifeFood";
-	}
-	@RequestMapping("/category/lifeHousehold")
-	public String lifeHousehold() {
-		
-		
-		
-		
-		
-		return "category/lifeHousehold";
-	}
-	
-	@ResponseBody
-	@RequestMapping("/category/laptopPcEnd.do")
-	public List<Regist> laptopPcEnd() {
-		List<Regist> list = categoryService.laptopPcEnd();
-		
-		return list;
-		
-	}
+	@Autowired
+	ItemService itemService;
 	
 	@RequestMapping("/")
 	public ModelAndView	categoryInHeader(ModelAndView mav) {
 //		첫 로딩시 category data를 header에 전달. interceptor / aop 가능성.
 		List<Map<String,String>> list = categoryService.selectMacro();
 		logger.debug(list);
+		List<Product> newList = itemService.selectNew();
 		
 		List<Map<String,String>> auctionList = auctionService.selectAuctionList();
 		System.out.println("경매물품리스트"+auctionList);
 
+		mav.addObject("cpList", newList);
 		mav.addObject("auctionList" , auctionList);
 		mav.addObject("categoryList",list);
 		mav.setViewName("index");
